f34726a4be8c18798eb6048f8bbf8eafe68de870
Arturo0214 <osvaldosuarezcruz@gmail.com>
2023-08-24 13:43:05 -0600

    feat: adding changes according to the frontend

--- backend/models/requestModel.js
+++ backend/models/requestModel.js
@@ -1,30 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const requestSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: true,
   },
   title: {
     type: String,
-    required: [true, 'Por favor, ingresa un título']
+    required: [true, 'Por favor, ingresa un título'],
   },
-  description: {
+  areaEstudios: {
     type: String,
-    required: [true, 'Por favor, ingresa una descripción']
+    enum: ['Area1', 'Area2', 'Area3', 'Area4'],
+    required: [true, 'Por favor, selecciona un área de estudios'],
   },
-  type: {
+  requerimientos: {
+    type: String, // Cambiado a String para almacenar el nombre del archivo
+    required: [true, 'Por favor, ingresa los requerimientos del trabajo de investigación'],
+  },
+  tipoTrabajo: {
     type: String,
-    required: [true, 'Por favor, ingresa un tipo de trabajo']
+    required: [true, 'Por favor, selecciona un tipo de trabajo'],
+  },
+  otroTipoTrabajo: String, // Campo opcional si se selecciona 'Otro' en tipoTrabajo
+  extension: {
+    type: Number,
+    required: [true, 'Por favor, ingresa la extensión del proyecto (cuartillas)'],
   },
   status: {
     type: String,
     enum: ['pendiente', 'en proceso', 'completado'],
-    default: 'pendiente'
-  }
+    default: 'pendiente',
+  },
 }, {
-  timestamps: true
-})
+  timestamps: true,
+});
 
-module.exports = mongoose.model('Request', requestSchema)
+module.exports = mongoose.model('Request', requestSchema);
